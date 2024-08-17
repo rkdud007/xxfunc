b50b04478736a4c608e1e34d8f7353a664f0810a
Ammar Arif <evergreenkary@gmail.com>
2024-08-17 12:07:44 -0700

    restructure crates

---
+++ xxfunc/macro/src/lib.rs
@@ -0,0 +1,11 @@
+mod xxfunc_proc;
+
+use proc_macro::TokenStream;
+use syn::{parse_macro_input, ItemFn};
+use xfunc_proc::xxfunc_proc;
+
+#[proc_macro_attribute]
+pub fn main(_attr: TokenStream, input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as ItemFn);
+    xxfunc_proc(input).into()
+}
