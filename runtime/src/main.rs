3f4fa7055c0ce5cb7a1eb8957ee635494cd2a839
Ammar Arif <evergreenkary@gmail.com>
2024-08-16 18:37:03 -0700

    wip

--- runtime/src/main.rs
+++ runtime/src/main.rs
@@ -8,10 +8,18 @@ use proto::execute_server::Execute;
 use proto::execute_server::ExecuteServer;
 use proto::{ExecuteRequest, ExecuteResponse};
 use tonic::{transport::Server, Request, Response, Status};
+use tracing::info;
 use wasm::Runtime;
 
 pub struct ExecuteService {
     runtime: Runtime,
+}
+
+impl ExecuteService {
+    pub fn new() -> Result<Self> {
+        let runtime = Runtime::new()?;
+        Ok(ExecuteService { runtime })
+    }
 }
 
 #[tonic::async_trait]
@@ -20,19 +28,18 @@ impl Execute for ExecuteService {
         &self,
         request: Request<ExecuteRequest>,
     ) -> Result<Response<ExecuteResponse>, Status> {
-        let req = request.into_inner();
+        info!("Executing function");
+        let _ = request.into_inner();
         // TODO: Implement the actual execution logic here
-        let result = format!("Executed {} with {} bytes of data", req.name, req.data.len());
-        Ok(Response::new(ExecuteResponse { result }))
+        Ok(Response::new(ExecuteResponse {}))
     }
 }
 
-pub async fn run_server() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     let addr = "[::1]:50051".parse()?;
 
-    let runtime = Runtime::new()?;
-    let service = ExecuteService { runtime };
-
+    let service = ExecuteService::new()?;
     Server::builder().add_service(ExecuteServer::new(service)).serve(addr).await?;
 
     Ok(())
