b76d12c840e9e9d6194487ef34e9c6e550359e11
Pia <gayeongparkk@gmail.com>
2024-08-17 16:28:37 -0700

    mock notification

--- exex/src/exex/reth.rs
+++ exex/src/exex/reth.rs
@@ -1,4 +1,8 @@
-use tokio::sync::mpsc;
+use std::{sync::Arc, time::Duration};
+
+use reth_execution_types::Chain;
+use reth_exex::ExExNotification;
+use tokio::{sync::mpsc, time};
 
 use super::{
     rpc::{ExExRpcExt, ExExRpcExtApiServer},
@@ -7,7 +11,10 @@ use super::{
 
 pub fn init_reth() -> eyre::Result<()> {
     reth::cli::Cli::parse_args().run(|builder, _| async move {
-        let (rpc_tx, rpc_rx) = mpsc::unbounded_channel();
+        let (rpc_tx, _) = mpsc::unbounded_channel();
+
+        // create fake notification channel to controll notification sender
+        let (notification_sender, notification_receiver) = mpsc::channel(100);
 
         let handle = builder
             .node(reth_node_ethereum::EthereumNode::default())
@@ -15,9 +22,34 @@ pub fn init_reth() -> eyre::Result<()> {
                 ctx.modules.merge_configured(ExExRpcExt { to_exex: rpc_tx }.into_rpc())?;
                 Ok(())
             })
-            .install_exex("xx", |mut ctx| async { Ok(Scheduler::new(ctx)?.start()) })
+            .install_exex("xx", |mut ctx| async {
+                // override notification receiver
+                ctx.notifications = notification_receiver;
+
+                Ok(Scheduler::new(ctx)?.start())
+            })
             .launch()
             .await?;
+
+        // Start a task to send mock notifications every 10 seconds
+        tokio::spawn(async move {
+            let mut interval = time::interval(Duration::from_secs(10));
+            loop {
+                interval.tick().await;
+                let notification = ExExNotification::ChainCommitted {
+                    new: Arc::new(Chain::from_block(
+                        Default::default(),
+                        Default::default(),
+                        Default::default(),
+                    )),
+                };
+                if let Err(e) = notification_sender.send(notification).await {
+                    eprintln!("Failed to send notification: {}", e);
+                    break;
+                }
+                println!("Sent mock notification");
+            }
+        });
 
         handle.wait_for_node_exit().await
     })
